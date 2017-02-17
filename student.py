d4524e7f4a70e60748b0fce49fa29c971ce274b6
HarrTrev <harringtont@gilmour.org>
2017-02-17 11:00:29 -0500

    Robot controls  v.5.5 final dance

--- student.py
+++ student.py
@@ -76,6 +76,8 @@ class GoPiggy(pigo.Pigo):
         for x in range(3):
             time.sleep(.2)
         self.encR(100)
+        time.sleep(.3)
+        self.encL(100)
 
 
     def sprinkler(self):
