c107eda2e601bd6ceca7257e901d66bb8740542c
HarrTrev <harringtont@gilmour.org>
2017-04-12 10:41:52 -0400

    changed back to insert and added a 0 for i in ...insert(i, x)

--- student.py
+++ student.py
@@ -214,7 +214,7 @@ class GoPiggy(pigo.Pigo):
         #not seeing a path
         path_detected = False
         #list to store angles
-        direction = []
+        self.direction = []
         #loop through found angles
         for x in self.scan:
             #check if the angle is good
@@ -222,7 +222,7 @@ class GoPiggy(pigo.Pigo):
                 #angle good save angle good then save
                 if not path_detected:
                     #save angle at start
-                    direction.add(x)
+                    self.direction.insert(0, x)
                 path_detected = True
             #if bad angle
             else:
@@ -231,7 +231,7 @@ class GoPiggy(pigo.Pigo):
                     #good angle end
                     path_detected = False
                     #insert end angle
-                    direction.add(x)
+                    self.direction.insert(0, x)
         #print angles
         print ("Good angles are: " + str(direction))
 
